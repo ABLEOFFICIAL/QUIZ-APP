07adb0380972eda91a25183c72c44af36fe99f2f
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-24 11:56:09 -0800

    WIP

--- quiz.js
+++ quiz.js
@@ -21,29 +21,39 @@ const questions = [
     },
   ];
   
-var quizContainer = document.getElementById("quiz-container");
-var quizButton = document.getElementById("quiz-btn");
-var submitButton = document.getElementById("submit-btn");
+let quizContainer = document.getElementById("quiz-container");
+let quizButton = document.getElementById("quiz-btn");
+let submitButton = document.getElementById("submit-btn");
 
 function displayQuiz(questions){
+  let currentQuestion;
     questions.forEach((qst, index) => {
-        var currentQuestion = document.createElement('div')// create a question container
+        currentQuestion = document.createElement('div')// create a question container
         currentQuestion.classList.add('div-style');//add a class to the container
 
-        var questionText = document.createElement('h2');
-        questionText.classList.add('questions')
-        questionText.innerHTML = `${index + 1}. ${qst.question}`;
+        let questionText = document.createElement('h2');//create a question text element
+        questionText.classList.add('questions')//add class to question text
+        questionText.innerHTML = `${index + 1}. ${qst.question}`;//add the question text value
         currentQuestion.appendChild(questionText);
 
         let answerList = document.createElement('ul');
         answerList.classList.add('answer-list');
         qst.answers.forEach(answer => {
             let answerItem = document.createElement('li');
-            answerItem.classList.add('answers');
+        WIP    answerItem.classList.add('answers');
             answerItem.innerHTML = answer;
             answerList.appendChild(answerItem);
-        })
+        }) 
         currentQuestion.appendChild(answerList);
+    })
+    quizContainer.appendChild(currentQuestion);
+    let quizBtn = document.createElement('button');
+    quizBtn.classList.add('quiz-btn');
+    quizBtn.innerHTML = "Next";
+    quizContainer.appendChild(quizBtn);
+
+    quizBtn.addEventListener('click', function(){
+
     })
 }
 displayQuiz(questions);
@@ -107,17 +117,17 @@ displayQuiz(questions);
 
 
 /*function displayQuiz(){
-    var currentQuestion = questions[questionIndexNo];
+    let currentQuestion = questions[questionIndexNo];
     questionIndexNo = questionIndexNo + 1;
-    var questionElement = document.createElement('h2');
+    let questionElement = document.createElement('h2');
     questionElement.classList.add('questions');
     questionElement.textContent = questionIndexNo + ". " + currentQuestion.question;
     quizContainer.appendChild(questionElement);
 
-    var answerList = document.createElement('ul');
+    let answerList = document.createElement('ul');
     answerList.classList.add('answer-list');
     currentQuestion.answers.forEach(answer => {
-        var answerElement = document.createElement('li');
+        let answerElement = document.createElement('li');
         answerElement.classList.add('answers');
         answerElement.textContent = answer;
         answerList.appendChild(answerElement);
@@ -125,7 +135,7 @@ displayQuiz(questions);
     quizContainer.appendChild(answerList);
 }*/
 
-startQuiz();
+// startQuiz();
 
 
 
@@ -166,16 +176,16 @@ startQuiz();
 /*
     questions.forEach((q, index) => {
         // question
-        var quizQuestion = document.createElement('h2');// creating a question
+        let quizQuestion = document.createElement('h2');// creating a question
         quizQuestion.classList.add('questions');//add a class to question
         quizQuestion.textContent = `${index + 1}. ${q.question}`;// add text to question
         quizContainer.appendChild(quizQuestion);// add question to container
 
         // answers
-        var answersContainer = document.createElement('ul');//creting a ul-list for answers
+        let answersContainer = document.createElement('ul');//creting a ul-list for answers
         answersContainer.classList.add('answer-list');
         q.answers.forEach((answer) => {
-            var answerList = document.createElement('li')// creating answe list
+            let answerList = document.createElement('li')// creating answe list
             answerList.classList.add('answers');
             answerList.textContent = answer;
             answersContainer.appendChild(answerList);
