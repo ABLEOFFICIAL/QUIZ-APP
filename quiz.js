2929f792f93f269ada2fd52da2e3ccc67771021c
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-30 13:38:44 +0100

    learning

--- quiz.js
+++ quiz.js
@@ -24,7 +24,6 @@ const questions = [
 let quizContainer = document.getElementById("quiz-container");
 let qst = document.getElementById('questions');
 let answerList = document.getElementById('answer-list');
-// let quizButton = document.getElementById("quiz-btn");
 
 //init values
 let currentQuestionIndex = 0;
@@ -33,16 +32,63 @@ let score = 0;
 let onLoad = () => {
   score = 0;
   currentQuestionIndex = 0
-  let quizButton = document.createElement('button');
-  quizButton.classList.add('quiz-btn');
-  quizButton.innerHTML = 'Next';
   displayQuizQst();
 }
 
 let displayQuizQst = () => {
-  let currentQuestionIndex = currentQuestionIndex + 1;
+  //to display the questions
+  let qstNo = currentQuestionIndex + 1;
   let currentQuestion = questions[currentQuestionIndex];
-  qst.innerHTML = currentQuestionIndex +'. '+ currentQuestion;
+  qst.innerHTML = qstNo +'. '+ currentQuestion.question;
+
+  //clearing previous output
+  answerList.innerHTML = '';
+
+  //displaying answers
+  currentQuestion.answers.forEach(answer => {
+    let answerElement = document.createElement('li');
+    answerElement.classList.add('answers');
+    answerElement.textContent = answer;
+    answerList.appendChild(answerElement);
+
+    //select answers
+    answerElement.addEventListener('click', (e) => {
+      e.target.style.backgroundColor = '#6dfa3e';
+    //check if answer is correct
+      let correctAnswer = currentQuestion.correctAnswer;
+      let selectedAnswer = e.target.textContent;
+      if (selectedAnswer === correctAnswer) {
+        score++;
+      }else{
+        score = score;
+      }
+
+      let quizButton = document.createElement('button');
+      quizButton.classList.add('quiz-btn');
+      quizButton.innerHTML = 'Next';
+      quizContainer.appendChild(quizButton);
+    })
+  })
+
+  //add eventListener to button
+  let quizButton = document.querySelector('.quiz-btn');
+  quizButton.addEventListener('click', () => {
+
+    //move to next question
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      displayQuizQst();
+    }else{
+      displayResult();
+    }
+  })
+  function displayResult() {
+    //display result
+    let result = document.createElement('div');
+    result.classList.add('result');
+    result.innerHTML = `Your score is ${score} out of ${questions.length}`;
+    quizContainer.appendChild(result);
+  }
 }
 
 onLoad();
